409c3be5e371341ec01a15748a31320040909e2f
Pedro H. Cisz <46607761+will1310@users.noreply.github.com>
2024-11-05 10:31:37 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const tasks = [
+  {title: "Comprar comida para o gato", type: "Urgente"},
+  {title: "Consertar Computador", type: "Importante"},
+  {title: "Beber água", type: "Normal"},
+  {title: "Enviar relatório trimestral", type: "Importante"},
+  {title: "Fazer exercícios físicos", type: "Normal"},
+  {title: "Agendar consulta médica", type: "Urgente"},
+  {title: "Ler pelo menos um capítulo de um livro", type: "Normal"},
+  {title: "Limpar a despensa", type: "Importante"},
+  {title: "Pagar a conta de energia", type: "Urgente"},
+  {title: "Assistir a um documentário interessante", type: "Normal"},
+];
